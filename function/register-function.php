05e657627ec7df4c42ef6b41610dba7b106c1be7
Ezequiel Gonzalez <ezequiel.gonzalez.cics@ust.edu.ph>
2024-01-24 19:36:44 +0800

    Register With Email Verification No Validation Yet

---
+++ function/register-function.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+//database connection
+include('../dbcon.php');
+
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+function sendemail_verify($fname,$email,$verify_token)
+{
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();   
+    $mail->SMTPAuth   = true;                             
+
+    $mail->Host       = 'smtp.gmail.com';                                    
+    $mail->Username   = 'cdemailverify@gmail.com';                    
+    $mail->Password   = 'imse cgjh qyzq bwhg';    
+    $mail->SMTPSecure ="tls";
+      
+    $mail->Port       = 587;           
+    
+    $mail->setFrom("cdemailverify@gmail.com", $fname);
+    $mail->addAddress($email);
+
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Email Verification';
+
+    $email_template = "<h2>You have registered</h2>
+ <h5>Verify your email address to login <h5>
+ <br/>
+ <a href ='http://localhost/SE2-FINALPROJECT/function/verify-email.php?token=$verify_token'>Click Me</a>";
+
+
+    $mail->Body = $email_template;
+    $mail->send();
+    if(!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        echo "Message has been sent";
+    }
+    
+    
+}
+
+
+//gets the input from the form
+if(isset($_POST['register_btn']))
+{
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+    $address = $_POST['address'];
+    $password = $_POST['password'];
+    $verify_token= md5(rand());
+
+ //Email exists or not
+ $check_email_query = "SELECT email FROM users WHERE email='$email' LIMIT 1";
+ $check_email_query_run = mysqli_query($con, $check_email_query);
+
+ if(mysqli_num_rows($check_email_query_run) > 0)
+ {
+         $_SESSION['status'] = "Email  already exists";
+        header("Location: ../register.php");
+ }
+ else
+ {
+     //Register user data
+     $query ="INSERT INTO users (fname, lname, email, contact, address, password, verify_token) VALUES ('$fname','$lname','$email','$contact','$address','$password','$verify_token')"; 
+     $query_run = mysqli_query($con, $query);
+
+     if($query_run)
+     {
+        sendemail_verify("$fname","$email", "$verify_token");
+
+
+         $_SESSION['status'] = "Registration Succesful! Verify your Email";
+         header("Location: ../register.php");
+     }
+     else
+     {
+         $_SESSION['status'] = "Registration Failed";
+         header("Location: ../register.php");
+     }
+ }
+
+   
+}
+?>
---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="../css/navbar.css" />
+    <link rel="stylesheet" href="./css/homepageStyle.css" />
+    <title>Home</title>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"
+    ></script>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inika&family=Plus+Jakarta+Sans&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <?php include('common/navbar.php');?>
+
+   <!-- Code of homepage -->
+
+    <!-- top part of the homepage after the navbar -->
+    <div class="topdiv">
+      <img src="./img/logo.png" alt="Logo" />
+      <a href="#" class="order-button">Order Now</a>
+    </div>
+
+    <!-- middle part where how to order is located -->
+    <div class="orderdiv">
+      <h1>How to Order</h1>
+      <div class="stepdiv">
+        <div class="elementcontainer">
+          <a href="#" class="circularnumber">1</a>
+          <h5>Login/Create Account</h5>
+        </div>
+        <div class="elementcontainer">
+          <img src="./img/user.png" alt="User" />
+        </div>
+      </div>
+      <div class="stepdiv">
+        <div class="elementcontainer">
+          <a href="#" class="circularnumber">2</a>
+          <h5>Select Items from Menu</h5>
+        </div>
+        <div class="elementcontainer">
+          <img src="./img/menu.png" alt="Menu" />
+        </div>
+      </div>
+      <div class="stepdiv">
+        <div class="elementcontainer">
+          <a href="#" class="circularnumber">3</a>
+          <h5>Checkout & Pay</h5>
+        </div>
+        <div class="elementcontainer">
+          <img src="./img/pay.png" alt="Pay" />
+        </div>
+      </div>
+    </div>
+
+    <!-- last part of the homepage -->
+    <div class="bottomdiv">
+      <div class="textandpiccontainer">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+          hendrerit libero odio, eget volutpat neque dapibus in. Suspendisse
+          mattis nec eros et sollicitudin. Vivamus ornare, justo in venenatis
+          accumsan, velit nibh viverra urna,
+        </p>
+        <img src="./img/pic1.jpg" alt="Pic1" />
+      </div>
+
+      <div class="textandpiccontainer">
+        <img src="./img/pic2.jpg" alt="Pic2" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+          hendrerit libero odio, eget volutpat neque dapibus in. Suspendisse
+          mattis nec eros et sollicitudin. Vivamus ornare, justo in venenatis
+          accumsan, velit nibh viverra urna,
+        </p>
+      </div>
+    </div>
+
+    <!-- End Code -->
+  </body>
+</html>
