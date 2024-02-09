1dcc89fe0b4815af8e3664d33c6223dd6970d647
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-02-09 23:40:55 +0800

    ./ deleted

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ include('./function/authentication.php')
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="../css/navbar.css" />
-    <link rel="stylesheet" href="./css/homepageStyle.css" />
+    <link rel="stylesheet" type="text/css" href="css/navbar.css" />
+    <link rel="stylesheet" href="css/homepageStyle.css" />
     <title>Home</title>
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
--- login.php
+++ login.php
@@ -7,8 +7,8 @@ session_start();
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="./css/navbar.css" />
-    <link rel="stylesheet" type="text/css" href="./css/register.css" />
+    <link rel="stylesheet" type="text/css" href="css/navbar.css" />
+    <link rel="stylesheet" type="text/css" href="css/register.css" />
     <title>Login</title>
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
