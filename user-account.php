76bc8594c2796f901d49d9b3dc48702081bd8779
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-04-10 03:56:32 +0800

    pay button fixed

--- user-account.php
+++ user-account.php
@@ -327,9 +327,9 @@ if ($result->num_rows == 1) {
                     ?>
                             <tr>
                                 <td class="orderno">Order No. <?php echo $orderId; ?></td>
-                                <td class="orderstat"><?php echo $orderStatus; ?></td>
+                                <td class="orderstat"><?php echo $orderStatus; ?></td>r
                                 <td class="checkout-btn">
-                                    <center><a href="checkout2.php?id=<?php echo $orderId; ?>" class="add-item mt-5"><i class="fa-solid fa-money-bill-wave" style="color:#004225;"></i> Checkout</a></center>
+                                    <center><a href="checkout2.php?id=<?php echo $orderId; ?>" class="add-item"  style="font-size: 12px"><i class="fa-solid fa-money-bill-wave" style="color:#004225;"></i> Checkout</a></center>
                                 </td>
                             </tr>
                             <?php
